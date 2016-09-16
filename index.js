c6fcb07da32c110a11d591e0c70cb5c09413cb2a
Vitaly Nikitin <vit.nikitin@gmail.com>
2016-09-16 23:20:59 +0300

    add custom dict

---
+++ index.js
@@ -0,0 +1,33 @@
+var fs = require("fs");
+var path = require('path');
+var country = require('country-data');
+var lookup = country.lookup;
+var dicts = [];
+
+var normalizedPath = path.join(__dirname, './', 'dicts');
+fs.readdirSync(normalizedPath).forEach(function (file) {
+    if (/\.dict\./.test(file)) {
+        var content = require(path.join(normalizedPath, file));
+        dicts.push(content);
+    }
+});
+
+var convert = function (countryName) {
+    var res = lookup.countries({
+        name: countryName
+    });
+    if (res.length === 0) {
+        for (var i = 0; i < dicts.length; i++) {
+            var dict = dicts[i];
+            res = lookup.countries({
+                name: dict[countryName]
+            });
+            if (res) break;
+        }
+    }
+    return (res[0] || {}).alpha2;
+};
+
+module.exports = {
+    convert: convert
+};
